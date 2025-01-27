c69a2e3fe690786767294785c8ad8245bdcb9df5
Santhosh <sandy592ff@gmail.com>
2025-01-27 16:09:03 +0530

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+function info(){
+    let name = document.getElementById("name").value
+    let age = Number(document.getElementById("age").value)
+    let address = document.getElementById("add").value
+    let result = document.getElementById("result")
+    result.innerHTML = "hi "+ name +" and your age is"+ age +"and the address is  "+address
+}
+console.log(naem)

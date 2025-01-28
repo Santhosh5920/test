b3c9ff6f6abfab54df777bf7b6d0d0f2c35c0dd9
Santhosh <sandy592ff@gmail.com>
2025-01-28 12:43:31 +0530

    done

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
+passwords.innerHTML =" password"
 function log(){
+    let passt = document.getElementById("passwords").value
     let uname = document.getElementById("name").value
     let pass = document.getElementById("pass").value
-    if(uname=="" && pass ==""){
-        alert("please fill username and password")
-    }
-    else if(uname==""){
-        alert("username must be filled")
-    }
-    else if(pass==""){
-        alert("password must be filled")
-    }
+    let validate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    alert(passt)
+    // if(uname=="" && pass ==""){
+    //     alert("please fill username and password")
+    // }
+    // else if(uname==""){
+    //     alert("username must be filled")
+    // }
+    // else if(pass==""){
+    //     alert("password must be filled")
+    // }
 }

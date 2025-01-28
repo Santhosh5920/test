240bd44d0846665334585ef5cdbfc6c26bab3da8
Santhosh <sandy592ff@gmail.com>
2025-01-28 16:58:10 +0530

    done

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-passwords.innerHTML =" password"
 function log(){
-    let passt = document.getElementById("passwords").value
-    let uname = document.getElementById("name").value
+    let uname = document.getElementById("username").value
     let pass = document.getElementById("pass").value
-    let validate = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-    alert(passt)
-    // if(uname=="" && pass ==""){
-    //     alert("please fill username and password")
-    // }
-    // else if(uname==""){
-    //     alert("username must be filled")
-    // }
-    // else if(pass==""){
-    //     alert("password must be filled")
-    // }
+    let uinput= document.getElementById("user-input")
+    let passinput= document.getElementById("pass-input")
+    if(uname!=""&& pass !=""){
+        uinput.innerHTML=" "
+        passinput.innerHTML=" "
+        if(pass.length<8){
+            passinput.innerHTML="The password must have 8 charaters !"
+        }
+        else if(pass.search(/[a-z]/)<0){
+            passinput.innerHTML="The password must contain a lowercase !"
+        }
+        else if(pass.search(/[A-Z]/)<0){
+            passinput.innerHTML="The password must contain a uppercase !"
+        }
+        else if(pass.search(/[0-9]/)<0){
+            passinput.innerHTML="The password must contain a number !"
+        }
+    }
+    else  if (uname=="" && pass==""){
+        uinput.innerHTML="Enter a username"
+        passinput.innerHTML="Enter a password"
+    }
+
+    else if(uname!="" && pass==""){
+        passinput.innerHTML="Enter a password"
+        uinput.innerHTML=" "
+    }
+    else if(uname=="" && pass!=""){
+        uinput.innerHTML="Enter a username"
+        passinput.innerHTML=" "
+    }
 }
+
